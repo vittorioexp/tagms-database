293be46ba092eceb94deae87367a0badb3897ef0
SamueleZanini <samuele.zanini@studenti.unipd.it>
2021-12-06 14:36:48 +0000

    update populate.sql

--- fdb-homework3-template/insert.sql
+++ fdb-homework3-template/insert.sql
@@ -5,3 +5,13 @@ insert into tagms.employee values ('LGNLVT85M04C227R','Paolo','Verdi','(0039)340
 insert into tagms.contract (description,contract_date,delivery_date,expiration_date,supplier_id,employee_id) values ('contract with supplier 1','2020-01-18','2021-12-06','2023-12-31',16835498612,'DKLERT23D96A316T');
 insert into tagms.contract (description,contract_date,delivery_date,expiration_date,supplier_id,employee_id) values ('contract with supplier 1','2021-02-01','2021-12-08','2024-12-31',16835498612,'MHVHQK62H26L404I');
 insert into tagms.contract (description,contract_date,delivery_date,expiration_date,supplier_id,employee_id) values ('contract with supplier 2','2019-01-25','2021-12-07','2022-12-31',16835498612,'MHVHQK62H26L404I');
+
+
+
+-- Role
+INSERT INTO Tagms.role(Role_ID, Name,Description) VALUES 
+('01','Manager','The managers are in charge of interacting with suppliers and concluding contracts with them');
+('02','Sellers','The sellers have to deal with customers and create orders');
+('03','Data analyst','The data analyst has access to the acquired data for inventory, cost and profit analysis');
+('04','System administrator','The system administrator manages the access privileges for each user on the system');
+('05','Worker','The workers take care of production and shipments');
