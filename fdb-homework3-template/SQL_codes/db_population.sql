b4911d973012cade7d3972270ec2c23f9c00deab
Vittorio Esposito <esposito.vittorio@rocketmail.com>
2021-12-07 17:56:45 +0100

    add some random tuples in made_up_of_1 and 2 in db_population.sql to test the principal queries

--- fdb-homework3-template/SQL_codes/db_population.sql
+++ fdb-homework3-template/SQL_codes/db_population.sql
@@ -166,15 +166,13 @@ Package_Quantity,Package_ID, Product_ID) VALUES
 
 -- TODO: Add many IN A WAY THAT MAKES SENSE, Martin
 -- Made_Up_Of_1
-INSERT INTO Tagms.made_Up_Of_1(Product_ID, Item_ID,Quantity) VALUES
-('1','1','2');
+INSERT INTO tagms.made_up_of_1 VALUES ('1','2','5'), ('1','3','10'), ('2', '2', '13');
 
 
 
 -- TODO: Add many IN A WAY THAT MAKES SENSE, Martin
 -- Made_Up_Of_2
-INSERT INTO Tagms.made_Up_Of_2(Package_ID, Item_ID, Quantity) VALUES
-('1','1','2');
+INSERT INTO tagms.made_up_of_2 VALUES ('1','2','5'), ('1','3','10'), ('2', '2', '13');
 
 
 -- Insert customers
