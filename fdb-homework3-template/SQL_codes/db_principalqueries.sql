6734011736deaa725b7edede7cf7d21042cbcdad
Vittorio Esposito <esposito.vittorio@rocketmail.com>
2021-12-07 18:21:49 +0100

    add query to list available lots (based on some criteria) in db_principalqueries.sql

--- fdb-homework3-template/SQL_codes/db_principalqueries.sql
+++ fdb-homework3-template/SQL_codes/db_principalqueries.sql
@@ -1,13 +1,16 @@
-SELECT t1.attr1
+-- TODO: what's this?
+/*SELECT t1.attr1
 FROM table1 as t1
          LEFT JOIN table2 as t2 ON t1.attr2=t2.attr1
-WHERE t1.attr3=1
+WHERE t1.attr3=1*/
 
 -- list all contracts between a certain manager and a certain supplier
 select tagms.contract.* from tagms.contract inner join tagms.employee on
         tagms.contract.employee_id=tagms.employee.tax_number inner join tagms.supplier on
-        tagms.contract.supplier_id=tagms.supplier.vat_number where tagms.employee.first_name='Luca' and
-        tagms.supplier.supplier_name='supplier1';
+        tagms.contract.supplier_id=tagms.supplier.vat_number where tagms.employee.first_name='Adam' and
+        tagms.supplier.supplier_name='Reg s.r.l.';
+
+
 
 -- list all outbound transactions sorted from most to least amount of money:
 select tagms.order.* from tagms.order order by tagms.order.net_price DESC;
@@ -29,13 +32,14 @@ select tagms.customer.* from tagms.customer;
 
 
 
+
 --After inserting a new lot with identifier Lot_id (see the previous chapter), execute step 1
 
 -- Step 1: Get the list of items to be decremented given a Lot_id.
 SELECT tagms.made_up_of_1.item_id FROM tagms.lot
                                            JOIN tagms.made_up_of_1 ON
     (tagms.lot.product_id = tagms.made_up_of_1.product_id)
-WHERE tagms.lot.lot_id = '1'
+WHERE tagms.lot.lot_id = '1';
 
 -- For each item returned by step 1, execute step 2.
 
@@ -44,11 +48,26 @@ WHERE tagms.lot.lot_id = '1'
 UPDATE tagms.item
 SET quantity = quantity - (
     SELECT tagms.lot.product_quantity * tagms.made_up_of_1.quantity AS decrement_qty FROM tagms.lot
-                                                                                              JOIN tagms.made_up_of_1 ON
-        (tagms.lot.product_id = tagms.made_up_of_1.product_id AND tagms.made_up_of_1.item_id = '1')
+        JOIN tagms.made_up_of_1 ON
+            (tagms.lot.product_id = tagms.made_up_of_1.product_id AND tagms.made_up_of_1.item_id = '1')
     WHERE tagms.lot.lot_id = '1'
 )
 WHERE tagms.item.item_id=1;
 
+
+-- Lists all available (unsold and that won't expire in 6 months) lots stocking
+-- a particular product with Product_id identifier,
+-- sorted by expiration date (oldest lots must be sold first).
+
+SELECT * FROM tagms.lot WHERE
+        tagms.lot.product_id = '1'
+            AND
+        (SELECT COUNT(Tagms.draws_from.lot_id)
+            FROM tagms.draws_from
+            WHERE tagms.draws_from.lot_id = '1'
+            ) = 1
+            AND
+        tagms.lot.expiration_date > (current_date + interval '6 months')
+    ORDER BY tagms.lot.expiration_date ASC;
 
 
