61f1260e86fedfe880b29d9452261521caba7243
Marco <marco.basso.10@studenti.unipd.it>
2021-12-08 14:14:06 +0100

    Added plastic/glass item and item category

--- fdb-homework3-template/SQL_codes/db_population.sql
+++ fdb-homework3-template/SQL_codes/db_population.sql
@@ -39,6 +39,12 @@ INSERT INTO Tagms.item_Category(name, description) VALUES
 INSERT INTO Tagms.item_Category(name, description) VALUES
 ('Aluminum','Aluminum employed for packaging');
 
+INSERT INTO Tagms.item_Category(name, description) VALUES
+('Plastic','Plastic employed for packaging');
+
+INSERT INTO Tagms.item_Category(name, description) VALUES
+('Glass','Glass employed for packaging');
+
 
 
 -- Package_Category
@@ -134,7 +140,13 @@ INSERT INTO Tagms.item(name, description, quantity, minimum_quantity, item_categ
 ('Pellets 4','Polystyrene (type 4) is an excellent pellet that ensures great resistance against shocks','800','150','6');
 
 INSERT INTO Tagms.item(name, description, quantity, minimum_quantity, item_category_id) VALUES
-('Aluminum','Aluminum with internal coating (coating) which aims to minimize the interaction between the beverage and aluminum','500','130','7');
+('Aluminum 1','Aluminum (type 1) with internal coating (coating) which aims to minimize the interaction between the beverage and aluminum','500','130','7');
+
+INSERT INTO Tagms.item(name, description, quantity, minimum_quantity, item_category_id) VALUES
+('Platic Bottle 1','Plastic bottle (type 1) with heat-insulating','200','1100','8');
+
+INSERT INTO Tagms.item(name, description, quantity, minimum_quantity, item_category_id) VALUES
+('Glass Bottle 1','Glass bottle (type 1) with heat-insulating','260','150','9');
 
 -- TODO: Check if works
 -- Package
