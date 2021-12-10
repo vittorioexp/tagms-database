d3f0701cf67258d5469598719db299b89526d606
Vittorio Esposito <esposito.vittorio@rocketmail.com>
2021-12-10 07:30:02 +0100

    start with jdbc

---
+++ fdb-homework3-template/Code/jdbc.java
@@ -0,0 +1,11 @@
+import java.sql.DriverManager ;
+import java.sql.ResultSet ;
+import java.sql.SQLException ;
+import java.sql.Statement ;
+
+public class Tagms {
+    public static void main(String[] args) {
+        System.out.println("test");
+
+    }
+}
