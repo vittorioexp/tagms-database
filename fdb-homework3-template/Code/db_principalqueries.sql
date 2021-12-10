ddc3e8bb320a1cfc7420f60d4591799876843811
Vittorio Esposito <esposito.vittorio@rocketmail.com>
2021-12-10 16:19:32 +0100

    various fixes in db_principalqueries.sql

--- fdb-homework3-template/Code/db_principalqueries.sql
+++ fdb-homework3-template/Code/db_principalqueries.sql
@@ -1,39 +1,45 @@
--- TODO: can we remove this?
-/*SELECT t1.attr1
-FROM table1 as t1
-         LEFT JOIN table2 as t2 ON t1.attr2=t2.attr1
-WHERE t1.attr3=1*/
 
 -- list all contracts between a certain manager and a certain supplier
-select c.description,c.contract_date,c.expiration_date,e.first_name as manager_name,e.last_name as manager_surname
-		from tagms.contract as c inner join tagms.employee as e on
-        c.employee_id=e.tax_number inner join tagms.supplier as s on
-        c.supplier_id=s.vat_number where e.tax_number='FGDVSF30C62D012T' and
-        s.supplier_name='Reg s.r.l.';
 
-
--- TODO: aren't the following 2 too simple?
--- list all outbound transactions sorted from most to least amount of money:
-select tagms.order.* from tagms.order order by tagms.order.net_price DESC;
-
--- list all inbound transactions sorted from most to least amount of money:
-select tagms.specify.* from tagms.specify order by tagms.specify.price DESC;
+SELECT c.description,
+       DATE(c.contract_date) AS contract_date,
+       DATE(c.expiration_date) AS expiration_date,
+       e.first_name AS manager_name,
+       e.last_name AS manager_surname
+		FROM tagms.contract AS c
+		    INNER JOIN tagms.employee AS e ON c.employee_id = e.tax_number
+		    INNER JOIN tagms.supplier AS s ON c.supplier_id = s.vat_number
+WHERE e.tax_number='FGDVSF30C62D012T'
+  AND s.supplier_name='Reg s.r.l.';
 
 
 
 
 -- Show all the details of the employees in the organization
 
-select tagms.employee.*, tagms.department.name from tagms.employee inner join tagms.work on
-        tagms.employee.tax_number=tagms.work.employee_id inner join tagms.department on
-        tagms.work.department_id=tagms.department.department_id where tagms.employee.still_working=TRUE;
+SELECT e.tax_number,
+       e.first_name,
+       e.last_name,
+       e.phone_number,
+       e.email_address,
+       e.birth_date,
+       e.hiring_date,
+       r.name,
+       d.name
+FROM tagms.employee AS e
+    INNER JOIN tagms.work AS w ON e.tax_number = w.employee_id
+    INNER JOIN tagms.department AS d ON w.department_id = d.department_id
+    INNER JOIN tagms.role AS r ON r.role_id = e.role_id
+WHERE e.still_working = TRUE;
+
+
 
 
 
 -- After inserting a new lot with identifier Lot_id (see the "populate" section)
 -- decrease the quantity of the items involved in the production of a lot
 -- Note: it is necessary to insert the Lot_id twice in the query
--- TODO: come evitare di ripetere due volte il lot_id = '3'?
+
 UPDATE tagms.item AS i SET
     quantity = c.quantity
 FROM (
@@ -47,25 +53,6 @@ FROM (
             INNER JOIN tagms.item AS i ON m2.item_id = i.item_id
         WHERE l.lot_id = '3'
      )
-    AS c(item_id, quantity)
-WHERE c.item_id = i.item_id
-RETURNING i.item_id, name, description, i.quantity, minimum_quantity, item_category_id;
-
-
--- In this query the output of the union is wrapped into another select to apply the WHERE condition on lot_id
--- Note that lot_id has been added in the two inner select
-UPDATE tagms.item AS i SET
-    quantity = c.quantity
-FROM (SELECT * FROM (
-    (SELECT i.item_id, i.quantity - l.product_quantity * m1.quantity AS quantity,l.lot_id FROM tagms.lot AS l
-        INNER JOIN tagms.made_up_of_1 AS m1 ON l.product_id = m1.product_id
-        INNER JOIN tagms.item AS i ON m1.item_id = i.item_id)
-    UNION
-    (SELECT i.item_id, i.quantity - l.package_quantity * m2.quantity AS quantity,l.lot_id FROM tagms.lot AS l
-        INNER JOIN tagms.made_up_of_2 AS m2 ON l.package_id = m2.package_id
-        INNER JOIN tagms.item AS i ON m2.item_id = i.item_id)) AS l
-    WHERE l.lot_id = '3'
-    ORDER BY item_id ASC)
     AS c(item_id, quantity)
 WHERE c.item_id = i.item_id
 RETURNING i.item_id, name, description, i.quantity, minimum_quantity, item_category_id;
@@ -90,6 +77,8 @@ RETURNING i.item_id, name, description, i.quantity, minimum_quantity, item_categ
 
 
 
+
+
 -- Lists all available lots (unsold and that won't expire in 6 months) containing
 -- a particular product having a given Product_id as identifier,
 -- sorted by expiration date (oldest lots must be sold first).
@@ -105,6 +94,7 @@ WHERE l.product_id = '6'
   AND l.expiration_date > (current_date + interval '6 months')
   AND df.order_id IS NULL
 ORDER BY l.expiration_date ASC;
+
 
 
 
@@ -130,7 +120,6 @@ SELECT SUM(p.production_cost * l.product_quantity) AS production_cost FROM tagms
     JOIN tagms.order AS o ON df.order_id = o.order_id
 WHERE DATE(o.order_date) >= '2021-01-01' AND DATE(o.order_date) <= '2021-12-31';
 
--- Note: BETWEEN was not used as it makes a strict comparison (extremes excluded)
 
 
 
@@ -153,6 +142,8 @@ WHERE o.order_id = '2'
 RETURNING o.order_id, o.net_price, o.taxes;
 
 
+
+
 -- When inserting a lot, the operator can choose a custom price
 -- or use the following query.
 -- Given a lot with Lot_id, compute the lot price
@@ -170,6 +161,8 @@ WHERE l.lot_id = '3'
 RETURNING *;
 
 
+
+
 -- Given an item (which is used to create a product) having Item_id as identifier
 -- and a time interval (actually, two dates),
 -- find the total quantity of that item that has been used for production or packaging during that time.
@@ -178,37 +171,32 @@ SELECT SUM(l.product_quantity * m1.quantity) AS quantity FROM tagms.made_up_of_1
     INNER JOIN tagms.lot AS l ON m1.product_id = l.product_id
     INNER JOIN tagms.draws_from AS df ON l.lot_id = df.lot_id
     INNER JOIN tagms.order AS o ON df.order_id = o.order_id
-WHERE m1.item_id = '9'
+WHERE m1.item_id = '1'
   AND DATE(o.order_date) >= '2021-01-01'
   AND DATE(o.order_date) <= '2021-12-31';
 
 
--- TODO: va bene la query precedente o bisogna proprio fare funzionare questa di seguito?
---  che in teoria potrebbe funzionare con qualsiasi tipo di item
-/*
-SELECT SUM(l.product_quantity * m.quantity) AS quantity FROM
-    (
-            SELECT * FROM tagms.made_up_of_1 AS m1
-        UNION
-            SELECT * FROM tagms.made_up_of_2 AS m2
-    )
-    AS m(product_id, item_id, quantity)
-    INNER JOIN tagms.lot AS l ON m.product_id = l.product_id
-    INNER JOIN tagms.draws_from AS df ON l.lot_id = df.lot_id
-    INNER JOIN tagms.order AS o ON df.order_id = o.order_id
-WHERE m.item_id = '8'
-  AND DATE(o.order_date) >= '2021-01-01'
-  AND DATE(o.order_date) <= '2021-12-31';
- */
 
--- TODO: query che ritorna gli items che sono sotto la soglia minima di giacenza (troppo facile?)
+-- Return the list of items which have stock quantity lower than the minimum one
 
 SELECT * FROM tagms.item AS i
 WHERE i.quantity < i.minimum_quantity;
 
+
+
 -- Return the list of unsellable lots which are in stock (that will expire in less than 6 months)
-SELECT * FROM tagms.lot AS l
+
+SELECT
+       l.lot_id,
+       DATE(l.expiration_date) AS expiration_date,
+       l.product_id,
+       l.product_quantity,
+       l.package_id,
+       l.package_quantity,
+       l.lot_discount,
+       l.vat,
+       l.lot_price
+       FROM tagms.lot AS l
     LEFT OUTER JOIN tagms.draws_from AS df ON l.lot_id = df.lot_id
 WHERE l.expiration_date <= (current_date + interval '6 months')
   AND df.order_id IS NULL;
-
