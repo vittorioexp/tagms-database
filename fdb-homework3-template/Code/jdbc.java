12a4077d526537a4c1861657fd45d4ad2c0b20f1
Vittorio Esposito <esposito.vittorio@rocketmail.com>
2021-12-10 07:38:21 +0100

    writing java code (2)

--- fdb-homework3-template/Code/jdbc.java
+++ fdb-homework3-template/Code/jdbc.java
@@ -33,6 +33,7 @@ public class Tagms {
      * @param args command - line arguments ( not used ).
      */
     public static void main(String[] args) {
+
         // the connection to the DBMS
         Connection con = null ;
         // the statement to be executed
@@ -45,5 +46,47 @@ public class Tagms {
         // end time of a statement
         long end;
 
+        // ” data structures ” for the data to be read from the database
+        try {
+            // register the JDBC driver
+            Class . forName ( DRIVER );
+            System . out . printf ("Driver %s successfully registered .%n", DRIVER );
+        }  catch ( ClassNotFoundException e ) {
+            System.out.printf(
+            "Driver % s not found : %s. % n",DRIVER, e.getMessage());
+            // terminate with a generic error code
+            System.exit(-1);
+        }
+
+        try {
+            // connect to the database
+            start = System.currentTimeMillis ();
+            con = DriverManager.getConnection (DATABASE , USER , PASSWORD);
+            end = System.currentTimeMillis ();
+            System.out.printf (
+            "Connection to database %s successfully established in % ,d milliseconds .%n",
+            DATABASE , end - start );
+            // create the statement to execute the query
+            start = System.currentTimeMillis();
+            stmt = con.createStatement ();
+            end = System.currentTimeMillis ();
+            System.out.printf (
+            "Statement successfully created in % ,d milliseconds .%n",
+            end - start);
+            // get all the employees
+            String sql ="SELECT * from Tagms.employee;";
+            start = System.currentTimeMillis ();
+            rs = stmt.executeQuery ( sql );
+            System.out.printf ("% nEvent List :%n");
+            String id;
+            String name;
+            String email;
+            // TODO
+            // cycle on the query results ( i . e . for each employee we will select the events to be printed )
+            while ( rs.next ()) {
+
+            }
+
+        }
     }
 }
